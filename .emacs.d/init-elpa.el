a3e029021822ea7c8ecb24f0ede29951d07d6b84
Deepak Tripathi <dtripathi@juniper.net>
2001-12-22 06:24:25 -0500

    Updated mepla server address and added autopair

--- .emacs.d/init-elpa.el
+++ .emacs.d/init-elpa.el
@@ -5,7 +5,7 @@
                          ("ELPA" . "http://tromey.com/elpa/") 
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          )
       )
@@ -26,6 +26,7 @@
 
 (require-package 'bbdb)
 (require-package 'erc)
+(require-package 'autopair)
 (require-package 'boxquote)
 (require-package 'auto-complete)
 (require-package 'yasnippet)
